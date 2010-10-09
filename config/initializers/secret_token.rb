5902a22435485f248cf8485068e63801cc933572
Chris Boertien <chris.boertien@gmail.com>
2010-10-08 21:08:11 -0500

    initial commit
    Signed-off-by: Chris Boertien <chris.boertien@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Refinery::Application.config.secret_token = '0a22acac5e16f860c26fa4c1e0b02d38272fee8df15bae6ba6b440b1adde2d2c8088d9e64e10151efd6751668c7998d456a27032a6663bc16e76838f8c2ceb18'
